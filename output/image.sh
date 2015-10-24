d8ced7739c640b8630ead2de5dfa978b9a2a65b1
Mohammed Sameer <msameer@foolab.org>
2015-10-24 23:32:11 +0300

    Use kpartx and losetup instead of the broken image we were generating for qemu

--- output/image.sh
+++ output/image.sh
@@ -1,13 +1,21 @@
 create_temp_dir() {
     dd if=/dev/null of=$IMG bs=2M seek=1024
-    echo -e "n\np\n\n\n\nw\n" | /sbin/fdisk $IMG
-    /sbin/mkfs.ext4 -L root -F $IMG
-    /sbin/tune2fs -c 0 -i 0 $IMG
+    echo -e "n\np\n\n\n\na\nw\n" | /sbin/fdisk $IMG
+    /sbin/losetup /dev/loop0 $IMG
+    /sbin/kpartx -as /dev/loop0
+    /sbin/mkfs.ext4 -F -L root /dev/mapper/loop0p1
+    /sbin/tune2fs -c 0 -i 0 /dev/mapper/loop0p1
     DIR=$(mktemp -d)
-    mount -t auto $IMG $DIR -oloop
+    mount -t auto /dev/mapper/loop0p1 $DIR -oloop
+}
+
+cleanup() {
+    umount $DIR
+    /sbin/kpartx -d /dev/loop0
+    /sbin/losetup -d /dev/loop0
+    rm -rf $DIR
 }
 
 create_output() {
-    umount $DIR
-    rm -rf $DIR
+    cleanup
 }
