851a96de93a263d1c1a6d72444e7be21df932884
Mohammed Sameer <msameer@foolab.org>
2015-11-03 00:23:51 +0200

    Use upstart instead of sysvinit-core

--- build_root.sh
+++ build_root.sh
@@ -50,7 +50,7 @@ echo "========================="
 echo "Build directory: $DIR"
 echo "========================="
 
-$DEBOOTSTRAP --variant=minbase --include=sysvinit-core --arch=$ARCH $DEBIAN_SUITE $DIR
+$DEBOOTSTRAP --variant=minbase --include=upstart --arch=$ARCH $DEBIAN_SUITE $DIR
 echo "proc /proc proc defaults 0 0" >> $DIR/etc/fstab
 echo "sysfs /sys sysfs defaults 0 0" >> $DIR/etc/fstab
 echo "deb http://security.debian.org jessie/updates main" >> $DIR/etc/apt/sources.list
